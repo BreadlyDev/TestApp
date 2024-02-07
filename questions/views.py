bf552a68bd7c082691895e05cd2ee7a13be16b4f
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-02-08 01:00:29 +0600

    added get questions by test endpoint

--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from . import models as m, serializers as s
 
 
@@ -11,6 +14,14 @@ class QuestionListAPIView(generics.ListAPIView):
     queryset = m.Question.objects.all()
     serializer_class = s.QuestionSerializer
 
+class QuestionsByTestAPIView(APIView):
+    def get(self, request, pk):
+        try:
+            questions = m.Question.objects.filter(test=pk)
+            serializer = s.QuestionSerializer(questions, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class QuestionDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = m.Question.objects.all()
