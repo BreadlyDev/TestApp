00390ba109f78b8d32d4fe0db123d2ee95d3f60c
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-02-08 01:00:08 +0600

    renamed urls for answers

--- questions/urls.py
+++ questions/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('create', views.QuestionCreateAPIView.as_view()),
     path('all', views.QuestionListAPIView.as_view()),
+    path('test/<int:pk>', views.QuestionsByTestAPIView.as_view()),
     path('<int:pk>', views.QuestionDetailAPIView.as_view()),
 
-    path('create', views.AnswerCreateAPIView.as_view()),
-    path('all', views.AnswerListAPIView.as_view()),
-    path('<int:pk>', views.AnswerDetailAPIView.as_view()),
+    path('answer/create', views.AnswerCreateAPIView.as_view()),
+    path('answer/all', views.AnswerListAPIView.as_view()),
+    path('answer/<int:pk>', views.AnswerDetailAPIView.as_view()),
 ]
