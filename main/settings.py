4b29cdfc2ae6a7c2bec6f45a4faeb1b8bbf27ff8
BreadlyDev <breadydev@gmail.com>
2024-02-08 13:06:17 +0000

    Some changes in settings and urls in main

--- main/settings.py
+++ main/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
-    'drf_yasg',
     'users',
     'news',
     'corsheaders',
--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,7 +21,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from main import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -47,8 +45,4 @@ urlpatterns = [
     path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-<<<<<<< HEAD
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
 ]
->>>>>>> 01adea6097ecf782fbbd062208c42d2abbeb330b
